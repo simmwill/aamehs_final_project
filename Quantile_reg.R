aa6266d105a89577c0084670d83162ba7c54ee6b
Liz <52462132+LizbethGomez@users.noreply.github.com>
2020-03-20 09:57:01 -0400

    progress on quantile reg

--- Quantile_reg.R
+++ Quantile_reg.R
@@ -17,6 +17,11 @@ demo_11 = haven::read_xpt('./data/DEMO_11_12.xpt')
 demo_13 = haven::read_xpt('./data/DEMO_13_14.xpt')
 demo_15 = haven::read_xpt('./data/DEMO_15_16.xpt')
 
+#Bisphenols
+pc_11 = haven::read_xpt('./data/EPH_11_12.xpt')           ## different dataset for bisphenols 11-12
+pc_13 = haven::read_xpt('./data/EPHPP_13_14.xpt')
+pc_15 = haven::read_xpt('./data/EPHPP_15_16.xpt')
+
 
 #Self-reported Thyroid function
 med_11 = haven::read_xpt('./data/MCQ_11_12.xpt')
@@ -25,7 +30,32 @@ med_15 = haven::read_xpt('./data/MCQ_15_16.xpt')
 
 #Merging by year
 nh1112<-merge(demo_11,med_11,by="SEQN",all=T)
-nh1314<-merge(demo_13,med_13,by="SEQN",all=T)
-nh1516<-merge(demo_15,med_15,by="SEQN",all=T)
+nh1112<-merge(nh1112, pc_11, by="SEQN", all=T)
 
-df <- read_csv(paste0(DataFolder, "2010_County_Data.csv"))
+nh1314<-merge(demo_13,med_13,by="SEQN",all=T)
+nh1112<-merge(nh1314, pc_13, by="SEQN", all=T)
+
+nh1516<-merge(demo_15,med_15,by="SEQN",all=T)
+nh1112<-merge(nh1516, pc_15, by="SEQN", all=T)
+
+#?rq 
+
+nh1112 %>% 
+  mutate(
+    id = SEQN,  
+    bpa_u = URXBPH,   
+    bpa_u_det = URDBPHLC,   
+    bpf_u = URXBPF,
+    bpf_u_det = URDBPFLC,
+    bps_u = URXBPS,
+    bps_u_det = URDBPSLC,
+    thyroid_problem = MCQ160M,  ## ever told you had thyroid problem
+    thyroid_current = MCQ170M)
+
+#Did we get any feedback about our binary outcome for quantile regression?
+
+#50% percentile -mean
+Mod50 <- rq(thyroid_problem ~ bpa_u + bpf_u + PerLatinx + PerAsianAm + 
+              MedHInc + MedHVal + LTHS + FemaleUnemp + MaleUnemp + ClimateRegion, 
+            data = df, 
+            tau = 0.5)
