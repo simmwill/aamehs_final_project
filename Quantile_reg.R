3d98cbc4bda2b99e9ae2aaced2aca90c28335012
Liz <52462132+LizbethGomez@users.noreply.github.com>
2020-03-25 22:34:02 -0400

    updates to QR

--- Quantile_reg.R
+++ Quantile_reg.R
@@ -13,57 +13,14 @@ OutputFolder <- "~/Desktop/CUMC-MPH/Spring 2020/ADVANCED METHODS/aamehs_final_pr
 
 #data import 
 # Maybe we should think about creating and exporting a dataset with all of our variables of iterest.
-demo_11 = haven::read_xpt('./data/DEMO_11_12.xpt')
-demo_13 = haven::read_xpt('./data/DEMO_13_14.xpt')
-demo_15 = haven::read_xpt('./data/DEMO_15_16.xpt')
 
-#Bisphenols
-pc_11 = haven::read_xpt('./data/EPH_11_12.xpt')           ## different dataset for bisphenols 11-12
-pc_13 = haven::read_xpt('./data/EPHPP_13_14.xpt')
-pc_15 = haven::read_xpt('./data/EPHPP_15_16.xpt')
+df = readRDS("./data/analytic_dataset.RDS")
 
 
-#Self-reported Thyroid function
-med_11 = haven::read_xpt('./data/MCQ_11_12.xpt')
-med_13 = haven::read_xpt('./data/MCQ_13_14.xpt')
-med_15 = haven::read_xpt('./data/MCQ_15_16.xpt')
-
-#Merging by year
-nh1112<-merge(demo_11,med_11,by="SEQN",all=T)
-nh1112<-merge(nh1112, pc_11, by="SEQN", all=T)
-
-nh1314<-merge(demo_13,med_13,by="SEQN",all=T)
-nh1314<-merge(nh1314, pc_13, by="SEQN", all=T)
-
-nh1516<-merge(demo_15,med_15,by="SEQN",all=T)
-nh1516<-merge(nh1516, pc_15, by="SEQN", all=T)
-
-#?rq 
-
-nh1112 %>% 
-  mutate(
-    id = SEQN,  
-    bpa_u = URXBPH,   
-    bpa_u_det = URDBPHLC,   
-    bpf_u = URXBPF,
-    bpf_u_det = URDBPFLC,
-    bps_u = URXBPS,
-    bps_u_det = URDBPSLC,
-    thyroid_problem = MCQ160M,  ## ever told you had thyroid problem
-    thyroid_current = MCQ170M)
-
-
-#Data export
-
-saveRDS(nh1112, file = "./data/nh1112.RDS")   
-saveRDS(nh1314, file = "./data/nh1314.RDS") 
-saveRDS(nh1516, file = "./data/nh1516.RDS") 
-
-#Did we get any feedback about our binary outcome for quantile regression?
 
 #50% percentile -mean
-Mod50 <- rq(thyroid_problem ~ bpa_u + bpf_u + PerLatinx + PerAsianAm + 
-              MedHInc + MedHVal + LTHS + FemaleUnemp + MaleUnemp + ClimateRegion, 
+Mod50 <- rq(thyroid_outcome ~ bpa_u + bpf_u + bps_u + creatinine_u + sex + 
+            age + race, 
             data = df, 
             tau = 0.5)
-
+summary(Mod50, alpha = 0.05)
